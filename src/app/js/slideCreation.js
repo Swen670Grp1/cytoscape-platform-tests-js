20b0198c9aebcaa64b9da12be6fd209e9557fa42
skatulich <skatulich@gmail.com>
2018-11-15 22:04:31 -0500

    Updates to add 2nd slide
    includes basic css

--- src/app/js/slideCreation.js
+++ src/app/js/slideCreation.js
@@ -12,12 +12,13 @@
     $('#blank').remove();
   
     // add some slides
-    newslide('Download the Cytoscape Milestone Release');
+    newslide('Download the <br> Cytoscape Milestone Release','<a href="https://github.com/cytoscape">Go to Github Releases</a>','The installer should create an executable for you');
     newslide('Close Session');
 })();
 
-function newslide(i){
+function newslide(firstline,secondline,thirdline){
   // wrap the new content in the blank
-  $('<em>' + '<br>'+i+'</em>').appendTo(deck)
+  $('<em>' + '<br>'+firstline+ '<br><br>' + secondline + '<br>' + thirdline+ '</em>').appendTo(deck)
                       .wrap( wrap );
+  $("em").css("font-size","25px");
 };
